31407f0f5f7e12c2381440883444c6bddca444c5
Yitao <s3843689@student.rmit.edu.au>
2020-09-26 19:36:58 +1000

    commit

---
+++ store.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Project</title>
+    <link rel="stylesheet" type="text/css" href="StorePackage/store.css">
+</head>
+
+<body>
+    <!--Reerence: The bootstrap is inspired by COSC2413 Web programming Shekhar Lecture5 Example 9.-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+
+    <?php require_once('fragment/nav.inc.php'); ?>
+
+    <div class="game1 box">
+        <img class=" box" src="BIS Project/game1.jpg" alt="GameImage1" width="200px" height="200px"> <br>
+        <span class="gameFont">Tony Hawk’s Pro Skater 1 + 2<br><span class="colorWhite">$69.95 </span>
+            <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
+                    type="text" value="" id="surname" /></div>
+    </div>
+
+    <div class="game2 box">
+        <img class="box" src="BIS Project/game2.jpg" alt="GameImage2" width="200px" height="200px"> <br>
+        <span class="gameFont">NBA 2K21 Mamba Forever Edition<br><span class="colorWhite">$149.95</span></span>
+        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
+                type="text" value="" id="surname" /></div>
+    </div>
+
+    <div class="game3 box">
+        <img class="box" src="BIS Project/game3.jpg" alt="GameImage3" width="200px" height="200px"> <br>
+        <span class="gameFont">Marvel's Avengers<br><span class="colorWhite">$99.95</span></span>
+        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
+                type="text" value="" id="surname" /></div>
+    </div>
+
+    <div class="game4 box">
+        <img class="box" src="BIS Project/game4.jpg" alt="GameImage4" width="200px" height="200px"> <br>
+        <span class="gameFont">NBA 2K21<br><span class="colorWhite">$99.95</span></span>
+        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
+                type="text" value="" id="surname" /></div>
+    </div>
+
+    <div class="game5 box">
+        <img class="box" src="BIS Project/game5.jpg" alt="GameImage4" width="200px" height="200px"> <br>
+        <span class="gameFont">Crash Bandicoot 4: It's About Time<br><span class="colorWhite">$99.95</span></span>
+        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
+                type="text" value="" id="surname" /></div>
+    </div>
+
+    <div class="game6 box">
+        <img class="box" src="BIS Project/game6.jpg" alt="GameImage4" width="200px" height="200px"> <br>
+        <span class="gameFont">Call of Duty®: Black Ops Cold War<br><span class="colorWhite">$109.95</span></span>
+        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
+                type="text" value="" id="surname" /></div>
+
+    </div>
+    <?php require_once('fragment/footer.inc.php'); ?>
+    </div>
+    </div>
+</body>
+
+</html>
