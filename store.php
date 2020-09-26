816c5dac2bbcd341ea2f62d4e2b4ad8e5cd313ed
Yitao <s3843689@student.rmit.edu.au>
2020-09-26 20:18:01 +1000

    link to other pages and fix the CSS

--- store.php
+++ store.php
@@ -1,71 +1,155 @@
 <!doctype html>
 <html lang="en">
+<?php require_once('fragment/functions.inc.php'); ?>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Project</title>
-    <link rel="stylesheet" type="text/css" href="StorePackage/store.css">
+    <?php require_once('fragment/head.inc.php'); ?>
 </head>
+
 <body>
-    <!--Reerence: The bootstrap is inspired by COSC2413 Web programming Shekhar Lecture5 Example 9.-->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
-    </script>
+    <style>
+    .MainPage {
+        display: grid;
+        grid-template-columns: auto auto auto auto auto auto;
+        grid-template-areas:
+            "game1 game1 game1 game2 game2 game2"
+            "game3 game3 game3 game4 game4 game4"
+            "game5 game5 game5 game6 game6 game6"
+            position: absolute;
+        width: 100%;
+        height: 100%;
+        margin: auto;
+        background-color: rgb(68 60 54 / 1);
+    }
 
+    .tempory {
+        grid-area: tempory;
+    }
+
+    body {
+        margin: 0px;
+    }
+
+    .box {
+        padding: 25px;
+        font-size: 100%;
+        color: white;
+    }
+
+
+    .ganme1 {
+        grid-area: game1;
+        text-align: center;
+    }
+
+    .ganme2 {
+        grid-area: game2;
+        text-align: center;
+    }
+
+    .ganme3 {
+        grid-area: game3;
+        text-align: center;
+    }
+
+    .ganme4 {
+        grid-area: game4;
+        text-align: center;
+    }
+
+    .ganme5 {
+        grid-area: game5;
+        text-align: center;
+    }
+
+    .ganme6 {
+        grid-area: game6;
+        text-align: center;
+    }
+
+    .gameFont {
+        color: wheat;
+        font-weight: bold;
+        font-size: large;
+    }
+
+
+    .gameFont {
+        color: wheat;
+        font-weight: bold;
+        font-size: large;
+    }
+
+    .colorWhite {
+        color: white;
+    }
+
+    .couponCode {
+        grid-area: coupon;
+        background-color: green;
+        color: white;
+        font-weight: bold;
+    }
+
+    .checkout {
+        background-color: red;
+        color: white;
+        font-weight: bold;
+    }
+    </style>
     <?php require_once('fragment/nav.inc.php'); ?>
+    <div class="MainPage">
 
-    <div class="game1 box">
-        <img class=" box" src="BIS Project/game1.jpg" alt="GameImage1" width="200px" height="200px"> <br>
-        <span class="gameFont">Tony Hawk’s Pro Skater 1 + 2<br><span class="colorWhite">$69.95 </span>
-            <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
-                    type="text" value="" id="surname" /></div>
-    </div>
 
-    <div class="game2 box">
-        <img class="box" src="BIS Project/game2.jpg" alt="GameImage2" width="200px" height="200px"> <br>
-        <span class="gameFont">NBA 2K21 Mamba Forever Edition<br><span class="colorWhite">$149.95</span></span>
-        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
-                type="text" value="" id="surname" /></div>
-    </div>
+        <div class="game1 box">
+            <img class=" box" src="StorePackage/game1.jpg" alt="GameImage1" width="200px" height="200px"> <br>
+            <span class="gameFont">Tony Hawk’s Pro Skater 1 + 2<br><span class="colorWhite">$69.95 </span>
+                <span class="couponCode">Coupon Code applied</span><br><br><br>
+                <div><a class="box checkout" href="purchase.php" target="_parent">Check Out</a></div>
+        </div>
 
-    <div class="game3 box">
-        <img class="box" src="BIS Project/game3.jpg" alt="GameImage3" width="200px" height="200px"> <br>
-        <span class="gameFont">Marvel's Avengers<br><span class="colorWhite">$99.95</span></span>
-        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
-                type="text" value="" id="surname" /></div>
-    </div>
+        <div class="game2 box">
+            <img class="box" src="StorePackage/game2.jpg" alt="GameImage2" width="200px" height="200px"> <br>
+            <span class="gameFont">NBA 2K21 Mamba Forever Edition<br><span
+                    class="colorWhite">$149.95</span></span><br><br><br>
+            <div><a class="box checkout" href="purchase.php" target="_parent">Check Out</a></div>
 
-    <div class="game4 box">
-        <img class="box" src="BIS Project/game4.jpg" alt="GameImage4" width="200px" height="200px"> <br>
-        <span class="gameFont">NBA 2K21<br><span class="colorWhite">$99.95</span></span>
-        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
-                type="text" value="" id="surname" /></div>
-    </div>
+        </div>
 
-    <div class="game5 box">
-        <img class="box" src="BIS Project/game5.jpg" alt="GameImage4" width="200px" height="200px"> <br>
-        <span class="gameFont">Crash Bandicoot 4: It's About Time<br><span class="colorWhite">$99.95</span></span>
-        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
-                type="text" value="" id="surname" /></div>
-    </div>
+        <div class="game3 box">
+            <img class="box" src="StorePackage/game3.jpg" alt="GameImage3" width="200px" height="200px"> <br>
+            <span class="gameFont">Marvel's Avengers<br><span class="colorWhite">$99.95</span></span>
+            <span class="couponCode">Coupon Code applied</span><br><br><br>
+            <div><a class="box checkout" href="purchase.php" target="_parent">Check Out</a></div>
 
-    <div class="game6 box">
-        <img class="box" src="BIS Project/game6.jpg" alt="GameImage4" width="200px" height="200px"> <br>
-        <span class="gameFont">Call of Duty®: Black Ops Cold War<br><span class="colorWhite">$109.95</span></span>
-        <div class="couponCode"><span class="label">Coupon code:</span><input class="textinput" name="surname"
-                type="text" value="" id="surname" /></div>
+        </div>
+
+        <div class="game4 box">
+            <img class="box" src="StorePackage/game4.jpg" alt="GameImage4" width="200px" height="200px"> <br>
+            <span class="gameFont">NBA 2K21<br><span class="colorWhite">$99.95</span></span><br><br><br>
+            <div><a class="box checkout" href="purchase.php" target="_parent">Check Out</a></div>
+
+        </div>
+
+        <div class="game5 box">
+            <img class="box" src="StorePackage/game5.jpg" alt="GameImage4" width="200px" height="200px"> <br>
+            <span class="gameFont">Crash Bandicoot 4: It's About Time<br><span
+                    class="colorWhite">$99.95</span></span><br><br><br>
+            <div><a class="box checkout" href="purchase.php" target="_parent">Check Out</a></div>
+
+        </div>
+
+        <div class="game6 box">
+            <img class="box" src="StorePackage/game6.jpg" alt="GameImage4" width="200px" height="200px"> <br>
+            <span class="gameFont">Call of Duty®: Black Ops Cold War<br><span class="colorWhite">$109.95</span></span>
+            <span class="couponCode">Coupon Code applied</span><br><br><br>
+            <div><a class="box checkout" href="purchase.php" target="_parent">Check Out</a></div>
+
+
+        </div>
 
     </div>
     <?php require_once('fragment/footer.inc.php'); ?>
-    </div>
-    </div>
 </body>
 
 </html>
