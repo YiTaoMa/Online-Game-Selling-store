75c13ac44976dd2860d7345f6e303c9ed21580f6
HYX0327 <62193096+HYX0327@users.noreply.github.com>
2020-10-07 23:38:53 +0000

    Update X1.php

--- X1.php
+++ X1.php
@@ -194,7 +194,7 @@ span.price {
                 </div>
                 <br><br />
                 <label for="cvv">Coupon code</label>
-                <input type="text" id="cc" name="cc" >
+                <input type="text" id="cc" name="cc" required="required" placeholder="NO3-08-10-20" >
                 
 				<p>(20% off by coupon code) </p>
                 <p>LEGO Star Wars: The Skywalker Saga (Placeholder Price) <span class="price">$89.95 × 80%</span></p>
