4d4dffab45aa05a2df13083640381b18d2972d69
HYX0327 <62193096+HYX0327@users.noreply.github.com>
2020-10-07 23:31:07 +0000

    Update PC3.php

--- PC3.php
+++ PC3.php
@@ -194,7 +194,7 @@ span.price {
                 </div>
                 <br><br />
                 <label for="cvv">Coupon code</label>
-                <input type="text" id="cc" name="cc" >
+                <input type="text" id="cc" name="cc" placeholder="352" required="required">
                 
 				<p>(20% off by coupon code) </p>
                 <p>The Elder Scrolls Online: Greymoor Upgrade Collector's Edition <span class="price">$249.95 × 80%</span></p>
