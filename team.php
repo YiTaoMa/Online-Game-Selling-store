7add8df0b617748edb3d03c907b9fe36d11644d1
[Xin Lin] <[s3710267@student.rmit.edu.au]>
2020-10-08 08:05:41 +1100

    commi

--- team.php
+++ team.php
@@ -10,41 +10,44 @@
 
     <div class="container">
         <div class="jumbotron">
-            <p class="lead">
-            HD Games created 
-            </p>
-            
-        </div>
+        <h2>About Us</h2>
+            <p>HD Games was created in 2020 with a team of six people. </p>
+            <p>HD games is committed to becoming an interesting shopping website. HD games allows users to attract more customers by playing games and getting coupons while shopping. At the same time, users can enjoy relaxation time in our website.</p>
 
-        <h1>Our Team</h1>
+        <h1 style="text-align: center;">Our Team</h1>
         <br>
-        <div class="row">
-            <div class="col-md-6 col-lg-4 col-xl-3">
-                <img src="img/Fall Guys.jpg" alt="Fall Guys" width="230" height="150">
+        <div class="col" style="text-align: center;">
+            <div class="col-md-6 col-lg-4 col-xl-12">
                 <p>Name: Yuxiang Huang</p>
-                <p></p>
+                <p>MVF4 - Purchase Products</p>
             </div>
-            <div class="col-md-6 col-lg-4 col-xl-3">
-                <img src="img/Red Dead Redemption 2.jpg" alt="Red Dead Redemption 2" width="230" height="150">
+            
+            <div class="col-md-6 col-lg-4 col-xl-12">
                 <p>Name: Xin Lin</p>
+                <p>MVF1 - Login and Registration</p>
             </div>
-            <div class="col-md-6 col-lg-4 col-xl-3">
-                <img src="img/Grand Theft Auto V.jpg" alt="Grand Theft Auto V" width="230" height="150">
+            
+            <div class="col-md-6 col-lg-4 col-xl-12">
                 <p>Name: Baiwei Liu</p>
+                <p>MVF3 - Blog</p>
             </div>
-            <div class="col-md-6 col-lg-4 col-xl-3">
-                <img src="img/eFootball PES 2021.jpg" alt="eFootball PES 2021" width="230" height="150">
+            
+            <div class="col-md-6 col-lg-4 col-xl-12">
                 <p>Name: Wenlao Peng</p>
+                <p>MVF5 - Ranking List</p>
             </div>
-            <div class="col-md-6 col-lg-4 col-xl-3">
-                <img src="img/eFootball PES 2021.jpg" alt="eFootball PES 2021" width="230" height="150">
+            
+            <div class="col-md-6 col-lg-4 col-xl-12">
                 <p>Name: Yitao Ma</p>
+                <p>MVF6 - Game Selling Page </p>
             </div>
-            <div class="col-md-6 col-lg-4 col-xl-3">
-                <img src="img/eFootball PES 2021.jpg" alt="eFootball PES 2021" width="230" height="150">
+            
+            <div class="col-md-6 col-lg-4 col-xl-12">
                 <p>Name: Hang Yang</p>
+                <p>MVF2 - Online Game</p>
             </div>
         </div>
+    </div>
 
         <?php require_once('fragment/footer.inc.php'); ?>
     </div>
