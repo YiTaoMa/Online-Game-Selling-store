936ef645add0473acf7791d316bcdfe489f6c5bc
HYX0327 <62193096+HYX0327@users.noreply.github.com>
2020-10-07 23:34:19 +0000

    Update PC6.php

--- PC6.php
+++ PC6.php
@@ -193,7 +193,7 @@ span.price {
                 </div>
                 <br><br />
                 <label for="cvv">Coupon code</label>
-                <input type="text" id="cc" name="cc" placeholder="352" required="required">
+                <input type="text" id="cc" name="cc" required="required" placeholder="NO3-08-10-20">
                 
 				<p>(20% off by coupon code) </p>
                 <p>Sniper Ghost Warrior Contracts 2 <span class="price">$59.95 × 80%</span></p>
